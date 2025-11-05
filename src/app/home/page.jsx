9c44cbdac522f78feb8de1a7ca610aebf8dd7de8
Kajal Chandail <chandailkajal9@gmail.com>
2025-11-05 22:04:33 +0530

    one-section

---
+++ src/app/home/page.jsx
@@ -0,0 +1,18 @@
+import Header from '@/components/header';
+import Section1 from '@/components/homepage/section1';
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full'>
+      <div className='bg-gradient-to-r from-green-500 to-green-600 text-white py-2 px-4 text-center font-bold text-sm sm:text-base'>
+        WhatsApp Official API – Broadcast Messages, Automate, Engage & Close Deals Faster!
+      </div>
+      <Header/>
+      <Section1/>
+      
+    </div>
+  )
+}
+
+export default Home;
